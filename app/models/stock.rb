a8aa8c4059db6bb865884f93c286b5cae56eda88
xaverp <xaverp@iiw.org>
2022-10-20 18:06:00 +0000

    Adde symbol search params and page

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -4,6 +4,11 @@ class Stock < ApplicationRecord
             publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
             endpoint: 'https://cloud.iexapis.com/v1'
           )
-        client.quote(ticker_symbol).latest_price
+        # client.quote(ticker_symbol).latest_price
+        begin
+            new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.quote(ticker_symbol).latest_price)
+        rescue => exception
+            return nil
+        end
     end
 end
